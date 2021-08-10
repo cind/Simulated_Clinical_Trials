a1d3353b15014d0701e59523ca8b2894744e632a
adlang <adamglang96@gmail.com>
2021-08-10 16:25:59 -0700

    built unctions to run sample size estimation

--- adsim_funcs_data.R
+++ adsim_funcs_data.R
@@ -26,13 +26,205 @@ TimeSinceBaseline <- function(data, timecol) {
   return.list <- list()
   for(i in 1:length(subjlist)) {
     subj <- subjlist[[i]]
-    min.time <- min(subj[[timecol]])
+    min.time <- which()
     subj$new_time <- subj[[timecol]] - min.time
     return.list[[i]] <- subj
   }
   return.list <- do.call(rbind, return.list)
   return(return.list)
 }
+
+
+
+TimeSinceBaselineValidAmy <- function(data, timecol) {
+  subjlist <- split(data, data$RID)
+  return.list <- list()
+  for(i in 1:length(subjlist)) {
+    subj <- subjlist[[i]]
+    if(1 %in% subj$adas_csf_valid | 1 %in% subj$adas_pet_valid) {
+    val.amy   <- which(subj$adas_csf_valid == 1 | subj$adas_pet_valid == 1)
+    min.index <- min(val.amy)
+    time.min  <- min(subj[timecol][val.amy,])
+    subj$new_time <- subj[[timecol]] - time.min
+    subj <- subj[min.index:nrow(subj), ]
+    return.list[[i]] <- subj
+    } 
+  }
+  return.list <- do.call(rbind, return.list)
+  return(return.list)
+}
+
+ 
+
+CreateBaselineVar <- function(data, column.name) {
+  splitsubj <- split(data, data$RID)
+  comb.list <- list()
+  for(i in 1:length(splitsubj)) {
+    bline.name <- paste(column.name, "_bl", sep="")
+    subj <- splitsubj[[i]]
+    rows <- nrow(subj)
+    bline.row <- which.min(subj$new_time)
+    bline.val <- subj[column.name][bline.row,]
+    bline.vec <- rep(bline.val, rows)
+    subj[bline.name] <- bline.vec
+    comb.list[[i]] <- subj
+  }
+  comb.list <- do.call(bind_rows, comb.list)
+  return(comb.list)
+}
+
+
+SampleJointDistribution <- function(data, sampling.list, n) {
+    data.list    <- list()
+    sample.vec   <- sampling.list[[1]]
+    prob.level.1 <- sampling.list[[2]]
+    prob.level.2 <- sampling.list[[3]]
+    split.data   <- split(data, data[[sample.vec]])
+    df1      <- split.data[[1]]
+    df2      <- split.data[[2]]
+    df1$prob <- rep(prob.level.1, nrow(df1))
+    df2$prob <- rep(prob.level.2, nrow(df2))
+    df.final <- bind_rows(df1, df2)
+    test.sample <- sample(rownames(df.final), size = n, replace = FALSE, prob = df.final$prob)
+    df.final <- df.final[test.sample, ]
+    return(df.final)
+}
+
+summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
+                      conf.interval=.95, .drop=TRUE) {
+  library(plyr)
+  
+  # New version of length which can handle NA's: if na.rm==T, don't count them
+  length2 <- function (x, na.rm=FALSE) {
+    if (na.rm) sum(!is.na(x))
+    else       length(x)
+  }
+  
+  # This does the summary. For each group's data frame, return a vector with
+  # N, mean, and sd
+  datac <- ddply(data, groupvars, .drop=.drop,
+                 .fun = function(xx, col) {
+                   c(N    = length2(xx[[col]], na.rm=na.rm),
+                     mean = mean   (xx[[col]], na.rm=na.rm),
+                     sd   = sd     (xx[[col]], na.rm=na.rm)
+                   )
+                 },
+                 measurevar
+  )
+  
+  # Rename the "mean" column
+  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
+  
+  # Confidence interval multiplier for standard error
+  # Calculate t-statistic for confidence interval: 
+  # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
+  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
+  datac$ci <- datac$se * ciMult
+  
+  return(datac)
+}
+
+
+
+PullLongData <- function(baseline.data, long.data) { 
+  data.id <- baseline.data[["RID"]]
+  longdata <- subset(long.data, RID %in% data.id)
+  longdata$RID <- factor(longdata$RID)
+  return(longdata)
+  }
+
+
+
+
+
+PlotObsData <- function(data, formula.fixed, ylab) {
+  model.fit <- lme(fixed = as.formula(formula.fixed), 
+                   random = ~1|RID,
+                   data=data)
+  sumstat <- summarySE(data = data,
+                       measurevar = word(formula.fixed, 1), 
+                       groupvars = c("new_time"))
+  sumstat$min.se <- sumstat[["mean"]] - (1.96*sumstat$se)
+  sumstat$max.se <- sumstat[["mean"]] + (1.96*sumstat$se)
+  sumstat$new_time <- as.numeric(as.character(sumstat$new_time))
+  
+  gplot <- ggplot(sumstat, aes_string(x="new_time", y="mean")) + 
+    geom_errorbar(aes_string(ymin="min.se", ymax="max.se", width=.1)) + geom_line(group=1) + geom_point() + scale_x_discrete(name = "Weeks", limits=c(0, 6, 12, 24)) + ylab(ylab)
+  return.list <- list("model" = model.fit,
+                      "summary_stats" = sumstat,
+                      "plot" = gplot)
+  return(return.list)
+  
+}
+
+
+SampleSizeSimulation <- function(sim.data, formula, fcompare_str, efficacy=.5, breaks) {
+  all.rids <- levels(sim.data[["RID"]])
+  nrids <- nlevels(sim.data[["RID"]])
+  n.sample <- round(nrids/2)
+  treat.rids <- sample(all.rids, n.sample, prob = rep(.5, length(all.rids)), replace = FALSE)
+  base.rids <- subset(all.rids, all.rids %notin% treat.rids)
+  sim.data$treat <- rep(NA, nrow(sim.data))
+  treat.rows <- which(sim.data$RID %in% treat.rids)
+  base.rows <- which(sim.data$RID %in% base.rids)
+  sim.data["treat"][treat.rows,] <- 1
+  sim.data["treat"][base.rows,] <- 0
+  sim.data$treat <- factor(sim.data$treat)
+  sim.model <- lmer(as.formula(formula), data = sim.data)
+  sim.model.large <- sim.model
+  fixef(sim.model.large)["treat1"] <- 0
+  treat.ef <- fixef(sim.model.large)["new_time"] * efficacy
+  treat.ef <- treat.ef * -1
+  fixef(sim.model.large)["treat1:new_time"] <- treat.ef
+  sim_ext_class     <- extend(sim.model.large, along="RID", n=max(breaks))
+  p_curve_treat_sim <- powerCurve(sim_ext_class, test=fcompare(as.formula(fcompare_str)), along="RID", breaks=breaks)
+  summ_sim<- summary(p_curve_treat_sim)
+  summ_sim$mean <- summ_sim$mean*100
+  summ_sim$lower <- summ_sim$lower*100
+  summ_sim$upper <- summ_sim$upper*100
+  gplot.sim <- ggplot(data = summ_sim, aes(x=nlevels, y=mean)) +geom_errorbar(aes(ymin=lower, ymax=upper)) + geom_line() + geom_point() + scale_x_discrete(limits=breaks)
+  gplot.sim <- gplot.sim + xlab("Sample Size per Arm") + ylab("Statistical Power (%)")
+  return.list <- list("model" = sim.model.large,
+                      "power_curve_output" = p_curve_treat_sim,
+                      "summary_stats" = summ_sim,
+                      "power_curve_plot" = gplot.sim)
+  return(return.list)
+}
+
+
+CombineSimPlots <- function(nonenrich, enrich, limits) {
+  sumstats_non <- nonenrich$summary_stats
+  sumstats_enr <- enrich$summary_stats
+  sumstats_non$Group <- rep("Non-Enriched", nrow(sumstats_non))
+  sumstats_enr$Group <- rep("Enriched", nrow(sumstats_enr))
+  fullsumstats <- rbind(sumstats_enr, sumstats_non)
+  gplot.sim <- ggplot(data = fullsumstats, aes(x=nlevels, y=mean, colour=Group)) +geom_errorbar(aes(ymin=lower, ymax=upper)) + geom_line() + geom_point() + scale_x_discrete(limits=limits)
+  gplot.sim <- gplot.sim + xlab("Sample Size per Arm") + ylab("Statistical Power (%)")
+  return(list("plot"=gplot.sim, "fullstats"=fullsumstats))
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- sim_script.R
+++ sim_script.R
@@ -1,14 +1,80 @@
-library(simstudy)
+#left entorhinal ST24CV
+#left inferior temporal ST32CV
+#left middle  temporal ST40CV
+#left fusiform         ST26CV
+
+#right entorhinal ST83CV
+#right inferior temporal ST91CV
+#right middle  temporal ST99CV
+#right fusiform         ST85CV
+
+
+
+#LeftLateralVentricle (ST37); 
+#RightLateralVentricle (ST96); 
+#LeftInferiorLateralVentricle (ST30); 
+#RightInferiorLateralVentricle (ST89); 
+#ThirdVentricle (ST127); 
+#FourthVentricle (ST9); 
+#FifthVentrical (ST8); 
+#LeftChoroidPlexus (ST21);
+#RightChoroidPlexus (ST80)
+
+#CSFABETA cutpoint <= 254
+#CSFPTAU cutpoint >= 24
+#PETAMY cutpooint >= .78
+install.packages("simr")
+
+vol.ims <- c("ST103CV", "ST44CV", "ST29SV", "ST88SV", "ST10CV", 
+             "ST24CV", "ST32CV", "ST40CV", 
+             "ST26CV", "ST83CV", "ST91CV", 
+             "ST99CV", "ST85CV", "ST37SV", "ST96SV",
+             "ST30SV", "ST89SV", "ST127SV", "ST9SV", "ST21SV", "ST80SV")
+
+source.script <- FALSE
+
+#library(simstudy)
 library(ADNIMERGE)
+library(plyr)
 library(dplyr)
 library(furniture)
 library(lme4)
-adas_scores_1  <- read.csv("/Users/adamgabriellang/Desktop/clinical_trial_sim/Data/ADASSCORES.csv")
-adas_scores_23 <- read.csv("/Users/adamgabriellang/Desktop/clinical_trial_sim/Data/ADAS_ADNIGO23.csv")
-adas_scores_1  <- adas_scores_1[,c("RID", "VISCODE",  "TOTAL11")]
-adas_scores_23 <- adas_scores_23[,c("RID", "VISCODE2", "TOTSCORE")]
+library(nlme)
+library(simr)
+#library(MargCond)
+library(stringr)
+
+test.adas       <- read.csv("/Users/adamgabriellang/Desktop/clinical_trial_sim/Data/ADASSCORES.csv")
+adni_imaging    <- read.csv("/Users/adamgabriellang/Desktop/clinical_trial_sim/Data/unharmonized_freesurfer_imaging.csv")
+adas_scores_1   <- read.csv("/Users/adamgabriellang/Desktop/clinical_trial_sim/Data/ADASSCORES.csv")
+adas_scores_23  <- read.csv("/Users/adamgabriellang/Desktop/clinical_trial_sim/Data/ADAS_ADNIGO23.csv")
+adni_neuropsych <- read.csv("/Users/adamgabriellang/Desktop/clinical_trial_sim/Data/Neuropsychological (1)/UWNPSYCHSUM_03_09_21.csv")
+csf.upenn9      <- read.csv("/Users/adamgabriellang/Desktop/clinical_trial_sim/Data/Biospecimen_Results/UPENNBIOMK9_04_19_17.csv")
+csf.upenn10     <- read.csv("/Users/adamgabriellang/Desktop/clinical_trial_sim/Data/Biospecimen_Results/UPENNBIOMK10_07_29_19.csv")
+csf.upenn12     <- read.csv("/Users/adamgabriellang/Desktop/clinical_trial_sim/Data/Biospecimen_Results/UPENNBIOMK12_01_04_21.csv")
+neuropath.data  <- read.csv("/Users/adamgabriellang/Desktop/clinical_trial_sim/Data/NEUROPATH_05_17_21.csv")
+
+adas_scores_1   <- adas_scores_1[,c("RID", "VISCODE",  "TOTAL11")]
+adas_scores_23  <- adas_scores_23[,c("RID", "VISCODE2", "TOTSCORE")]
+adni_neuropsych <- adni_neuropsych[,c("RID", "VISCODE2", "ADNI_MEM", "ADNI_EF")]
+adni_imaging    <- adni_imaging[,c("RID", "VISCODE2", vol.ims)]
+csf.upenn9      <- csf.upenn9[,c("RID", "VISCODE2", "EXAMDATE",  "ABETA", "TAU", "PTAU", "COMMENT")]
+csf.upenn10      <- csf.upenn10[,c("RID", "VISCODE2", "DRAWDATE",  "ABETA42", "TAU", "PTAU", "NOTE")]
+csf.upenn12      <- csf.upenn12[,c("RID", "VISCODE2", "EXAMDATE",  "ABETA", "TAU", "PTAU", "NOTE")]
+
 colnames(adas_scores_23) <- c("RID", "VISCODE", "TOTAL11")
+colnames(adni_neuropsych)<- c("RID", "VISCODE", "ADNI_MEM", "ADNI_EF")
+colnames(adni_imaging)   <- c("RID", "VISCODE", vol.ims)
+colnames(csf.upenn9) <- colnames(csf.upenn10) <- colnames(csf.upenn12) <- c("RID", "VISCODE", "EXAMDATE", "ABETA", "TAU", "PTAU", "COMMENT")
 adas_scores              <- rbind(adas_scores_1, adas_scores_23)
+csf.data                 <- rbind(csf.upenn9, csf.upenn10, csf.upenn12)
+csf.data$EXAMDATE <- as.POSIXct(csf.data$EXAMDATE)
+csf.data                 <- csf.data[order(csf.data$RID, csf.data$EXAMDATE, decreasing = FALSE), ]
+rownames(csf.data)       <- 1:nrow(csf.data)
+csf.drop <- which(duplicated(csf.data[,c("RID", "VISCODE")]))
+image.drop <- which(duplicated(adni_imaging[,c("RID", "VISCODE")]))
+adni_imaging <- adni_imaging[-image.drop,]
+csf.data <- csf.data[-csf.drop,]
 adas_scores              <- adas_scores[order(adas_scores$RID, 
                                               adas_scores$VISCODE, 
                                               decreasing = FALSE), ]
@@ -16,59 +82,71 @@ column.keeps             <- c("RID", "DX", "COLPROT", "ORIGPROT",
                                "VISCODE", "EXAMDATE", "AGE", 
                                "PTGENDER", "PTEDUCAT", 
                                "APOE4", "CDRSB", "MMSE", 
-                               "M")
+                               "M", "mPACCtrailsB")
 adas_demog       <- adnimerge[,column.keeps]
 drops1           <- which(adas_scores$VISCODE == "")
 drops2           <- which(adas_scores$VISCODE == "uns1")
 adas_scores      <- adas_scores[-unique(union(drops1, drops2)), ]
 adas_merge_demog <- merge(adas_demog, adas_scores, by=c("RID", "VISCODE"))
-
-
-
 adas_merge_demog <- na.omit(adas_merge_demog)
-av45             <- read.csv("/Users/adamgabriellang/Desktop/clinical_trial_sim/Data/UCBERKELEYAV45_01_14_21.csv")
-av45.keeps       <- c("RID", "EXAMDATE", "VISCODE2", "SUMMARYSUVR_COMPOSITE_REFNORM")
-av45             <- av45[,av45.keeps]
-colnames(av45)   <- c("RID", "EXAMDATE", "VISCODE", "SUMMARYSUVR_COMPOSITE_REFNORM")
-av45$AmyPos      <- rep(NA, nrow(av45))
-pos              <- which(av45$SUMMARYSUVR_COMPOSITE_REFNORM >= .78)
-neg              <- which(av45$SUMMARYSUVR_COMPOSITE_REFNORM < .78)
-av45["AmyPos"][pos,]  <- 1
-av45["AmyPos"][neg,]  <- 0
+adas_merge_demog <- merge(adas_merge_demog, adni_neuropsych, by=c("RID", "VISCODE"), all = TRUE)
+adas_merge_demog <- merge(adas_merge_demog, adni_imaging, by=c("RID", "VISCODE"), all = TRUE)
+adas_merge_demog <- merge(adas_merge_demog, csf.data, by=c("RID", "VISCODE"), all = TRUE)
+adas_merge_demog <- merge(adas_merge_demog, neuropath.data, by="RID", all = TRUE)
+adas_merge_demog$EXAMDATE.x <- as.POSIXct(adas_merge_demog$EXAMDATE.x)
+adas_merge_demog <- adas_merge_demog[order(adas_merge_demog$RID, adas_merge_demog$EXAMDATE.x, decreasing = FALSE), ]
+
+
+av45                 <- read.csv("/Users/adamgabriellang/Desktop/clinical_trial_sim/Data/UCBERKELEYAV45_01_14_21.csv")
+av45.keeps           <- c("RID", "EXAMDATE", "VISCODE2", "SUMMARYSUVR_COMPOSITE_REFNORM")
+av45                 <- av45[,av45.keeps]
+colnames(av45)       <- c("RID", "EXAMDATE", "VISCODE", "SUMMARYSUVR_COMPOSITE_REFNORM")
+av45$AmyPos          <- rep(NA, nrow(av45))
 adas_merge_demog$RID <- factor(adas_merge_demog$RID)
-adas_merge_demog$EXAMDATE <- as.POSIXct(adas_merge_demog$EXAMDATE)
+adas_merge_demog$EXAMDATE.x <- as.POSIXct(adas_merge_demog$EXAMDATE.x)
 av45$EXAMDATE <- as.POSIXct(av45$EXAMDATE)
 av45$RID             <- factor(av45$RID)
-adas_merge_demog_av45 <- MergeSubjectTime(adas_merge_demog, av45, "RID", "EXAMDATE", "EXAMDATE")
-adas_merge_demog_av45$timediff <- abs(difftime(adas_merge_demog_av45$EXAMDATE.x, adas_merge_demog_av45$EXAMDATE.y, units = "days"))
-adas_merge_demog_av45$timediff <- adas_merge_demog_av45$timediff / 365
-adas_merge_demog_av45$valid <- rep(NA, nrow(adas_merge_demog_av45))
-adas_merge_demog_av45["valid"][which(adas_merge_demog_av45$timediff <= .5), ] <- 1
-adas_merge_demog_av45["valid"][which(adas_merge_demog_av45$timediff > .5), ] <- 0
-adas_merge_demog_av45 <- adas_merge_demog_av45[order(adas_merge_demog_av45$RID,
-                                                     adas_merge_demog_av45$EXAMDATE.x,
-                                                     decreasing = FALSE), ]
+adas_merge_demog     <- merge(adas_merge_demog, av45, by=c("RID", "VISCODE"), all = TRUE)
+names(adas_merge_demog)[names(adas_merge_demog) == 'EXAMDATE.x'] <- 'EXAMDATE_adnimerge'
+names(adas_merge_demog)[names(adas_merge_demog) == 'EXAMDATE.y'] <- 'EXAMDATE_csf'
+names(adas_merge_demog)[names(adas_merge_demog) == 'EXAMDATE'] <- 'EXAMDATE_pet'
+adas_merge_demog$AGE <- round(adas_merge_demog$AGE + (adas_merge_demog$M / 12), 1)
+adas_merge_demog$diff_adas_csf <- abs(difftime(adas_merge_demog$EXAMDATE_adnimerge, adas_merge_demog$EXAMDATE_csf, units = "weeks"))
+adas_merge_demog$diff_adas_pet <- abs(difftime(adas_merge_demog$EXAMDATE_adnimerge, adas_merge_demog$EXAMDATE_pet, units = "weeks"))
+adas_merge_demog$adas_csf_valid <- adas_merge_demog$adas_pet_valid <- rep(NA, nrow(adas_merge_demog))
+adas_merge_demog["adas_csf_valid"][which(adas_merge_demog$diff_adas_csf/52 <= .5), ] <- 1
+adas_merge_demog["adas_csf_valid"][which(adas_merge_demog$diff_adas_csf/52 > .5), ] <- 0
+adas_merge_demog["adas_pet_valid"][which(adas_merge_demog$diff_adas_pet/52 <= .5), ] <- 1
+adas_merge_demog["adas_pet_valid"][which(adas_merge_demog$diff_adas_pet/52 > .5), ] <- 0
 
-adas_merge_demog_av45     <- na.omit(adas_merge_demog_av45)
-adas_merge_demog_av45$RID <- factor(adas_merge_demog_av45$RID)
-adas_merge_demog_av45$M   <- as.numeric(adas_merge_demog_av45$M)
-adas_merge_demog_av45     <- TimeSinceBaseline(adas_merge_demog_av45, timecol = "M")
+adas.outcome.data <- adas_merge_demog[,c("RID", "VISCODE", "DX", "COLPROT", "ORIGPROT", 
+                                         "VISCODE", "EXAMDATE_adnimerge", "AGE", 
+                                         "PTGENDER", "PTEDUCAT", 
+                                         "APOE4", "CDRSB", "MMSE", "TOTAL11", 
+                                         "adas_pet_valid", "adas_csf_valid", "EXAMDATE_pet", "M",
+                                         "ABETA", "TAU", "PTAU", "SUMMARYSUVR_COMPOSITE_REFNORM")]
+rids.adas.outcome <- which(!is.na(adas.outcome.data$adas_pet_valid) | !is.na(adas.outcome.data$adas_csf_valid))
+rids.adas.outcome <- unique(adas.outcome.data["RID"][rids.adas.outcome,])
+adas.outcome.data <- subset(adas.outcome.data, RID %in% rids.adas.outcome)
+adas.outcome.data <- adas.outcome.data[order(adas.outcome.data$RID, adas.outcome.data$EXAMDATE_adnimerge, decreasing = FALSE), ]
+adas.drops <- which(is.na(adas.outcome.data$TOTAL11))
+adas.outcome.data <- adas.outcome.data[-adas.drops, ]
+adas.outcome.data$ABETA <- as.numeric(adas.outcome.data$ABETA)
+adas.outcome.data$AmyPos <- rep(NA, nrow(adas.outcome.data))
+rownames(adas.outcome.data) <- 1:nrow(adas.outcome.data)
+adas.outcome.amypos <- which(adas.outcome.data$SUMMARYSUVR_COMPOSITE_REFNORM >= 0.78 | adas.outcome.data$ABETA <= 254)
+adas.outcome.data["AmyPos"][adas.outcome.amypos, ] <- 1
+adas.outcome.amyneg <- which(adas.outcome.data$SUMMARYSUVR_COMPOSITE_REFNORM < 0.78 & adas.outcome.data$ABETA > 254)
+adas.outcome.data["AmyPos"][adas.outcome.amyneg, ] <- 0
 
-rows.t1 <- which(adas_merge_demog_av45$new_time==0 & adas_merge_demog_av45$valid==1)
-rows.t2 <- which(adas_merge_demog_av45$new_time==6)
-rows.t3 <- which(adas_merge_demog_av45$new_time==12)
-rows.t4 <- which(adas_merge_demog_av45$new_time==18)
-rows.t5       <- which(adas_merge_demog_av45$new_time==24)
-adas_keep     <- adas_merge_demog_av45[c(rows.t1, rows.t2, rows.t3, rows.t4, rows.t5), ]
-adas_keep$RID <- factor(adas_keep$RID)
-adas_keep     <-  adas_keep[order(adas_keep$RID,
-                                          adas_keep$new_time,
-                                          decreasing = FALSE), ]
-
-
+adas.outcome.data$RID <- factor(adas.outcome.data$RID)
+adas.outcome.data <- TimeSinceBaselineValidAmy(adas.outcome.data, "M")
+adas.outcome.data <- subset(adas.outcome.data, new_time <= 24 & new_time != 18)
+adas.outcome.data$RID <- factor(adas.outcome.data$RID)
+adas.outcome.data <- CreateBaselineVar(adas.outcome.data, "AmyPos")
 #Baseline cohort
 
-adas_baseline           <- subset(adas_keep, new_time == 0)
+adas_baseline           <- subset(adas.outcome.data, new_time == 0)
 adas_baseline$DX        <- factor(adas_baseline$DX)
 adas_baseline$APOE4     <- factor(adas_baseline$APOE4)
 adas_baseline$PTGENDER  <- factor(adas_baseline$PTGENDER)
@@ -82,11 +160,111 @@ adas_desc <- adas_baseline[,c("DX", "APOE4", "PTGENDER", "AGE", "TOTAL11",
 desc.table <- table1(adas_desc, splitby = ~ DX)$Table1
 desc.table <- desc.table[c(1, 6:nrow(desc.table)), ]
 
-
-
+adas_desc_time <- adas.outcome.data[,c("TOTAL11", "new_time", "DX")]
+adas_desc_time$new_time <- factor(adas_desc_time$new_time)
+desc.table.time <- table1(adas_desc_time, splitby = ~ new_time)$Table1
+desc.table.time <- desc.table.time[c(1:3, 9:12), ]
 
 #### model fitting
-model1.out <- lmer(TOTAL11 ~  -1 + factor(new_time) + SUMMARYSUVR_COMPOSITE_REFNORM*factor(AmyPos) + (1|RID), data = adas_keep)
-summary(model1.out)
 
-vcov(model1.out)
+
+base.mci1     <- subset(adas_baseline, DX=="MCI" & CDRSB>=.5 & MMSE >= 20 & MMSE <=26 & AGE>=50 & AGE <=85 & AmyPos==1)
+desc.table.base.mci <- base.mci1[,c("DX", "APOE4", "PTGENDER", "AGE", "TOTAL11", 
+                                      "MMSE", "CDRSB", "PTEDUCAT",
+                                      "AmyPos")]
+desc.table.base.mci <- table1(desc.table.base.mci)$Table1
+
+data.mci1     <- PullLongData(base.mci1, adas.outcome.data)
+
+
+
+data.mci1$new_time <- as.numeric(data.mci1$new_time)
+data.mci1$APOE4 <- factor(data.mci1$APOE4)
+all.rids <- levels(data.mci1[["RID"]])
+treat.rids <- sample(all.rids, 90, prob = rep(.5, length(all.rids)), replace = FALSE)
+base.rids <- subset(all.rids, all.rids %notin% treat.rids)
+data.mci1$treat <- rep(NA, nrow(data.mci1))
+treat.rows <- which(data.mci1$RID %in% treat.rids)
+base.rows <- which(data.mci1$RID %in% base.rids)
+data.mci1["treat"][treat.rows,] <- 1
+data.mci1["treat"][base.rows,] <- 0
+data.mci1$treat <- factor(data.mci1$treat)
+mod2 <- lmer(TOTAL11 ~ treat + new_time+ new_time*treat + (1|RID), data = data.mci1)
+mod2_large <- mod2
+summary(mod2_large)
+fixef(mod2_large)["treat1"] <- 0
+fixef(mod2_large)["treat1:new_time"] <- (0.136017*.5)
+summary(mod2_large)
+mod2_ext_class <- extend(mod2_large, along="RID", n=650)
+p_curve_treat_mod2 <- powerCurve(mod2_ext_class, test=fcompare(TOTAL11~new_time), along="RID", breaks = c(100, 200, 300, 400, 500, 600, 650))
+sim_time           <-  powerSim(mod2_large, fcompare(TOTAL11 ~ new_time), nsim=100)
+
+summary(sim_time)
+
+plot(p_curve_treat_mod2)
+this<- summary(p_curve_treat_mod2)
+this$mean <- this$mean*100
+this$lower <- this$lower*100
+this$upper <- this$upper*100
+mci.enrich.plot.1 <- ggplot(data = this, aes(x=nlevels, y=mean)) +geom_errorbar(aes(ymin=lower, ymax=upper)) + geom_line() + geom_point() + scale_x_discrete(limits=this$nlevels)
+mci.enrich.plot.1 <- mci.enrich.plot.1 + xlab("Sample Size per Arm") + ylab("Statistical Power (%)")
+mci.enrich.plot.1
+all.rids <- levels(adas.outcome.data$RID)
+nrids    <- nlevels(adas.outcome.data$RID)
+keeps.rids  <- sample(all.rids, 300, prob = rep(.5, length(all.rids)))
+unenriched.data<- subset(adas.outcome.data, RID %in% keeps.rids)
+unenriched.data$RID <- factor(unenriched.data$RID)
+
+unenriched.totall1<- SampleSizeSimulation(sim.data = unenriched.data, formula = "TOTAL11 ~ treat + new_time+ new_time*treat + (1|RID)",
+                                          fcompare_str = "TOTAL11~new_time", breaks = seq(100, 1000, by=100))
+data.mci1.totall11 <- SampleSizeSimulation(sim.data = data.mci1, formula = "TOTAL11 ~ treat + new_time+ new_time*treat + (1|RID)",
+                                          fcompare_str = "TOTAL11~new_time", breaks =  seq(100, 1000, by=100))
+mci1.plot.total11 <- CombineSimPlots(nonenrich = unenriched.totall1, enrich = data.mci1.totall11, limits = seq(100, 1000, by=100))
+
+unenriched.cdr <- SampleSizeSimulation(sim.data = unenriched.data, formula = "CDRSB ~ treat + new_time+ new_time*treat + (1|RID)",
+                                          fcompare_str = "CDRSB~new_time", breaks = seq(100, 800, by=100))
+data.mci1.cdr <- SampleSizeSimulation(sim.data = data.mci1, formula = "CDRSB ~ treat + new_time+ new_time*treat + (1|RID)",
+                                          fcompare_str = "CDRSB~new_time", breaks = seq(100, 800, by=100))
+mci1.plot.cdr <- CombineSimPlots(nonenrich = unenriched.cdr, enrich = data.mci1.cdr, limits = seq(100, 800, by=100))
+
+firstplot <- mci1.plot.total11$plot
+firstplot <- firstplot + labs(title = "ADAS-COG11") +geom_hline(yintercept = 80, linetype="dashed")
+firstplot
+
+secondplot <- mci1.plot.cdr$plot
+secondplot <- secondplot + labs(title = "CDRSB") +geom_hline(yintercept = 80, linetype="dashed")
+secondplot
+
+
+
+
+
+amci1.analyzed <- PlotObsData(data.mci1, formula.fixed = "TOTAL11 ~ new_time", ylab= "ADAS-COG11")
+mci1.analyzed
+mci1.analyzed.cdr<- PlotObsData(data.mci1, formula.fixed = "CDRSB ~ new_time", ylab= "CDRSB")
+mci1.analyzed.cdr
+
+
+base.mci2     <- subset(adas_baseline, DX=="MCI" & CDRSB>=.5 & MMSE >= 20 & MMSE <=26 & AGE>=50 & AGE <=65 & AmyPos==1)
+data.mci2     <- PullLongData(base.mci2, adas.outcome.data)
+mci2.analyzed <- PlotObsData(data.mci2, formula.fixed = "TOTAL11 ~ new_time", ylab= "ADAS-COG11")
+mci2.analyzed
+mci2.analyzed.cdr <- PlotObsData(data.mci2, formula.fixed = "CDRSB ~ new_time", ylab= "CDRSB")
+mci2.analyzed.cdr
+
+base.ad1     <- subset(adas_baseline, DX == "Dementia" & CDRSB >= 1 & MMSE >= 20 & MMSE <= 26 & AGE >=50 & AGE <= 85) 
+data.ad1     <- PullLongData(base.ad1, adas.outcome.data)
+ad1.analyzed <- PlotObsData(data.ad1,formula.fixed = "TOTAL11 ~ new_time", ylab= "ADAS-COG11")
+ad1.analyzed
+ad1.analyzed.cdr <- PlotObsData(data.ad1,formula.fixed = "CDRSB ~ new_time", ylab= "CDRSB")
+ad1.analyzed.cdr
+
+base.ad2     <- subset(adas_baseline, DX == "Dementia" & CDRSB >= 1 & MMSE >= 20 & MMSE <= 26 & AGE >=50 & AGE <= 65) 
+data.ad2     <- PullLongData(base.ad2, adas.outcome.data)
+ad2.analyzed <- PlotObsData(data.ad2,formula.fixed = "TOTAL11 ~ new_time", ylab= "ADAS-COG11")
+ad2.analyzed
+ad2.analyzed.cdr <- PlotObsData(data.ad2,formula.fixed = "CDRSB ~ new_time", ylab= "CDRSB")
+ad2.analyzed.cdr
+
+intervals(mod1)
+
