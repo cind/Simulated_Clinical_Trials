9001bc564507055158096136a3ea5005f63c2fe5
AdamLang96 <45542095+AdamLang96@users.noreply.github.com>
2021-09-09 17:15:19 -0700

    set up to re run simulation based on neuropath effect sizes from Boyle and block randomized treatment/control groups

---
+++ fitting_simulations.R
@@ -0,0 +1,187 @@
+aut.rows <- which(complete.cases(adas_merge_demog[,c("NPBRAAK", "NPNEUR", "NPTDPB", "NPTDPC", "NPTDPD", "NPTDPE",
+                                                     "NPLBOD", "NPAMY")]))
+imputed.rows <- which(complete.cases(adas_merge_demog[,c("TDP43", "LEWY", "CAA")]))
+keeps <- unique(union(aut.rows, imputed.rows))
+all.neuropat <- adas_merge_demog[keeps,]
+all.neuropat <- SetNeuroData(all.neuropat)
+
+for(i in 1:nrow(all.neuropat)) {
+  if(!is.na(all.neuropat["Amy_pos_path"][i,]) & all.neuropat["Amy_pos_path"][i,]==1) {
+    all.neuropat["AmyPos_full"][i,] <- 1
+  } else if(!is.na(all.neuropat["AmyPos"][i,]) & all.neuropat["AmyPos"][i,]==1) {
+    all.neuropat["AmyPos_full"][i,] <- 1
+  } else if(!is.na(all.neuropat["Amy_pos_path"][i,]) | !is.na(all.neuropat["AmyPos"][i,])) {
+    all.neuropat["AmyPos_full"][i,] <- 0
+  }
+}
+
+for(i in 1:nrow(all.neuropat)) {
+  if(!is.na(all.neuropat["TAU_pos_path"][i,]) & all.neuropat["TAU_pos_path"][i,]==1) {
+    all.neuropat["TauPos_full"][i,] <- 1
+  } else if(!is.na(all.neuropat["ptau_pos"][i,]) & all.neuropat["ptau_pos"][i,]==1) {
+    all.neuropat["TauPos_full"][i,] <- 1
+  } else if(!is.na(all.neuropat["TAU_pos_path"][i,]) | !is.na(all.neuropat["ptau_pos"][i,])) {
+    all.neuropat["TauPos_full"][i,] <- 0
+  }
+}
+
+all.neuropat$RID <- factor(all.neuropat$RID)
+all.neuropat <- CreateBaselineVar(all.neuropat, "M_vis", "AmyPos_full")
+all.neuropat <- CreateBaselineVar(all.neuropat, "M_vis", "TauPos_full")
+all.neuropat$autopsy <- rep(NA, nrow(all.neuropat))
+all.neuropat <- all.neuropat[complete.cases(all.neuropat[,c("DX_bl", "PTEDUCAT_bl", "PTGENDER_bl", "fulllewy", 
+                                                            "fulltdp43", "fullcaa", "CDGLOBAL_bl", 
+                                                            "AGE_bl", "MMSE_bl", "AmyPos_full_bl", "TauPos_full_bl")]), ]
+
+all.neuropat.bl <- subset(all.neuropat, M_vis == 0)
+all.neuropat.bl$RID <- factor(all.neuropat.bl$RID)
+
+all.neuropat$autopsy <- complete.cases(all.neuropat[,c("NPBRAAK", "NPNEUR", "NPTDPB", "NPTDPC", "NPTDPD", "NPTDPE",
+                                                       "NPLBOD", "NPAMY")])
+necc.cols.t11     <- c("RID", "M_vis", "TOTAL11")
+necc.cols.t13     <- c("RID", "M_vis", "TOTAL13")
+necc.cols.cdr     <- c("RID", "M_vis", "CDRSB")
+necc.cols.mmse    <- c("RID", "M_vis", "MMSE")
+necc.cols.mpacc   <- c("RID", "M_vis", "mPACCtrailsB")
+necc.cols.mem     <- c("RID", "M_vis", "ADNI_MEM", "ADNI_EF")
+necc.cols.image   <- c("RID", "M_vis", "VentricalSum", "LeftMeta", "RightMeta")
+
+all.neuropat$RID            <- factor(all.neuropat$RID)
+all.neuropat$fulllewy       <- factor(all.neuropat$fulllewy)
+all.neuropat$fulltdp43      <- factor(all.neuropat$fulltdp43)
+all.neuropat$fullcaa        <- factor(all.neuropat$fullcaa)
+all.neuropat$AmyPos_full_bl <- factor(all.neuropat$AmyPos_full_bl)
+all.neuropat$TauPos_full_bl <- factor(all.neuropat$TauPos_full_bl)
+all.neuropat$Amy_pos_path   <- factor(all.neuropat$Amy_pos_path)
+all.neuropat$TAU_pos_path   <- factor(all.neuropat$TAU_pos_path)
+all.neuropat$PTGENDER       <- factor(all.neuropat$PTGENDER)
+
+
+combined.11    <- all.neuropat[complete.cases(all.neuropat[,necc.cols.t11]),]
+combined.13    <- all.neuropat[complete.cases(all.neuropat[,necc.cols.t13]),]
+combined.cdr   <- all.neuropat[complete.cases(all.neuropat[,necc.cols.cdr]),]
+combined.mmse  <- all.neuropat[complete.cases(all.neuropat[,necc.cols.mmse]),]
+combined.mpacc <- all.neuropat[complete.cases(all.neuropat[,necc.cols.mpacc]),]
+combined.mem   <- all.neuropat[complete.cases(all.neuropat[,necc.cols.mem]),]
+combined.image <- all.neuropat[complete.cases(all.neuropat[,necc.cols.image]),]
+
+
+
+full.data.list <- list("ADAS11"      = combined.11,
+                       "ADAS13"      = combined.13,
+                       "CDRSB"       = combined.cdr,
+                       "MMSE"        = combined.mmse,
+                       "MPACC"       = combined.mpacc,
+                       "MEM"         = combined.mem,
+                       "EF"          = combined.mem) #use same dataset for ADNI EF and ADNI MEM
+
+full.data.list <- Map(QuickAdjust, full.data.list)
+
+control.cohort                     <- lapply(full.data.list, function(x)  subset(x, new_time==0 &  DX_bl=="CN" & AmyPos_full_bl==0 & TauPos_full_bl == 0 & CDGLOBAL_bl == 0 & fulllewy == 0 & fulltdp43 == 0 & fullcaa == 0))
+control.cohort.long                <- purrr::map2(control.cohort, full.data.list, PullLongData)
+
+mci.scen1.generic                  <- lapply(full.data.list, function(x)  subset(x, new_time==0 & DX_bl=="MCI" & AmyPos_full_bl==1 & AGE_bl >= 65 & AGE_bl <= 85 & CDGLOBAL_bl==0.5 & MMSE_bl >=24 & MMSE_bl <= 30))
+mci.scen1.generic.tplus            <- lapply(full.data.list, function(x)  subset(x, new_time==0 & DX_bl=="MCI" & AmyPos_full_bl==1 & TauPos_full_bl==1 &  AGE_bl >= 65 & AGE_bl <= 85 & CDGLOBAL_bl==0.5 & MMSE_bl >=24 & MMSE_bl <= 30))
+mci.scen1.earlyage                 <- lapply(full.data.list, function(x)  subset(x, new_time==0 & DX_bl=="MCI" & AmyPos_full_bl==1 & CDGLOBAL_bl == 0.5 & MMSE_bl >=24 & MMSE_bl <= 30 & AGE >=50 & AGE <= 65))
+
+mci.scen1.generic.long             <- purrr::map2(mci.scen1.generic, full.data.list, PullLongData)
+mci.scen1.generic.long.tplus       <- purrr::map2(mci.scen1.generic.tplus, full.data.list, PullLongData)
+mci.scen1.earlyage.long            <- purrr::map2(mci.scen1.earlyage, full.data.list, PullLongData)
+
+early.ad.scen1.generic            <- lapply(full.data.list, function(x)  subset(x, new_time==0 & (DX_bl=="Dementia" | DX_bl=="MCI") & AmyPos_full_bl==1 & AGE_bl>=65 & AGE_bl <= 85 & MMSE_bl >=20 & MMSE_bl <= 30 & (CDGLOBAL_bl==.5 | CDGLOBAL_bl==1)))
+early.ad.scen1.generic.tplus      <- lapply(full.data.list, function(x)  subset(x, new_time==0 & (DX_bl=="Dementia" | DX_bl=="MCI") & AmyPos_full_bl==1 & TauPos_full_bl==1 & AGE_bl>=65 & AGE_bl <= 85 & MMSE_bl >=20 & MMSE_bl <= 30 & (CDGLOBAL_bl==.5 | CDGLOBAL_bl==1)))
+early.ad.scen1.earlyage           <- lapply(full.data.list, function(x)  subset(x,  new_time==0 & (DX_bl=="Dementia" | DX_bl=="MCI") & AmyPos_full_bl==1 & AGE_bl >=55 & AGE_bl <= 65 & MMSE_bl >=20 & MMSE_bl <= 30 & (CDGLOBAL_bl==.5 | CDGLOBAL_bl==1)))
+
+early.ad.scen1.generic.long        <- purrr::map2(early.ad.scen1.generic, full.data.list, PullLongData)
+early.ad.scen1.generic.long.tplus  <- purrr::map2(early.ad.scen1.generic.tplus, full.data.list, PullLongData)
+early.ad.scen1.earlyage.long       <- purrr::map2(early.ad.scen1.earlyage, full.data.list, PullLongData)
+
+ad.scen1.generic                   <- lapply(full.data.list, function(x)  subset(x, new_time==0 & DX_bl=="Dementia" & AmyPos_full_bl==1 & CDGLOBAL_bl >= 1 & MMSE_bl >=20 & MMSE_bl <= 28 & AGE_bl >=65 & AGE_bl <= 85))
+ad.scen1.generic.tplus             <- lapply(full.data.list, function(x)  subset(x, new_time==0 & DX_bl=="Dementia" & AmyPos_full_bl==1 & TauPos_full_bl==1 & CDGLOBAL_bl >= 1 & MMSE_bl >=20 & MMSE_bl <= 28 & AGE_bl >=65 & AGE_bl <= 85))
+ad.scen1.earlyage                  <- lapply(full.data.list, function(x)  subset(x, new_time==0 & DX_bl=="Dementia" & AmyPos_full_bl==1 & CDGLOBAL_bl >= 1 & MMSE_bl >=20 & MMSE_bl <= 28 & AGE_bl >=55 & AGE_bl <= 65))
+
+ad.scen1.generic.long              <- purrr::map2(ad.scen1.generic, full.data.list, PullLongData)
+ad.scen1.generic.long.tplus        <- purrr::map2(ad.scen1.generic.tplus, full.data.list, PullLongData)
+ad.scen1.earlyage.long             <- purrr::map2(ad.scen1.earlyage, full.data.list, PullLongData)
+
+
+baseline.control.data <- list("control" = control.cohort)
+full.control.data <- list("control" = control.cohort.long)
+
+baseline.enriched.data  <- list("mci"              = mci.scen1.generic,
+                                "mci.tplus"        = mci.scen1.generic.tplus,
+                                "mci.earlyage"     = mci.scen1.earlyage, 
+                                "earlyad"          = early.ad.scen1.generic,
+                                "earlyad.tplus"    = early.ad.scen1.generic.tplus,
+                                "earlyad.earlyage" = early.ad.scen1.earlyage,
+                                "ad"               = ad.scen1.generic,
+                                "ad.tplus"         = ad.scen1.generic.tplus,
+                                "ad.earlyage"      = ad.scen1.earlyage)
+
+full.enriched.data <- list("mci"              = mci.scen1.generic.long,
+                           "mci.tplus"        = mci.scen1.generic.long.tplus,
+                           "mci.earlyage"     = mci.scen1.earlyage.long, 
+                           "earlyad"          = early.ad.scen1.generic.long,
+                           "earlyad.tplus"    = early.ad.scen1.generic.long.tplus,
+                           "earlyad.earlyage" = early.ad.scen1.earlyage.long,
+                           "ad"               = ad.scen1.generic.long,
+                           "ad.tplus"         = ad.scen1.generic.long.tplus,
+                           "ad.earlyage"      = ad.scen1.earlyage.long)
+
+
+strat.continuous <- replicate(7, c("PTEDUCAT_bl", "AGE_bl"), simplify = FALSE)
+
+control.formulas <- list("ADAS11" = "TOTAL11      ~ new_time + PTEDUCAT_bl + AGE_bl +PTGENDER_bl+  (1|RID)",
+                         "ADAS13" = "TOTAL13      ~ new_time + PTEDUCAT_bl + AGE_bl +PTGENDER_bl+  (1|RID)",
+                         "CDRSB"  = "CDRSB        ~ new_time + PTEDUCAT_bl + AGE_bl +PTGENDER_bl+  (1|RID)",
+                         "MMSE"   = "MMSE         ~ new_time + PTEDUCAT_bl + AGE_bl +PTGENDER_bl+  (1|RID)",
+                         "MPACC"  = "mPACCtrailsB ~ new_time + PTEDUCAT_bl + AGE_bl +PTGENDER_bl+  (1|RID)",
+                         "MEM"    = "ADNI_MEM     ~ new_time + PTEDUCAT_bl + AGE_bl +PTGENDER_bl+  (1|RID)",
+                         "EF"     = "ADNI_EF      ~ new_time + PTEDUCAT_bl + AGE_bl +PTGENDER_bl+  (1|RID)")
+
+
+
+enrichment.formulas <-  list(  "ADAS11"  = "TOTAL11      ~ new_time + PTEDUCAT_bl + AGE_bl + PTGENDER_bl +  fulllewy*new_time + fullcaa*new_time + fulltdp43*new_time + (1|RID)",
+                               "ADAS13"  = "TOTAL13      ~ new_time + PTEDUCAT_bl + AGE_bl + PTGENDER_bl +  fulllewy*new_time + fullcaa*new_time + fulltdp43*new_time + (1|RID)",
+                               "CDRSB"   = "CDRSB        ~ new_time + PTEDUCAT_bl + AGE_bl + PTGENDER_bl +  fulllewy*new_time + fullcaa*new_time + fulltdp43*new_time + (1|RID)",
+                               "MMSE"    = "MMSE         ~ new_time + PTEDUCAT_bl + AGE_bl + PTGENDER_bl +  fulllewy*new_time + fullcaa*new_time + fulltdp43*new_time + (1|RID)",
+                               "MPACC"   = "mPACCtrailsB ~ new_time + PTEDUCAT_bl + AGE_bl + PTGENDER_bl +  fulllewy*new_time + fullcaa*new_time + fulltdp43*new_time + (1|RID)",
+                               "MEM"     = "ADNI_MEM     ~ new_time + PTEDUCAT_bl + AGE_bl + PTGENDER_bl +  fulllewy*new_time + fullcaa*new_time + fulltdp43*new_time + (1|RID)",
+                               "EF"      = "ADNI_EF      ~ new_time + PTEDUCAT_bl + AGE_bl + PTGENDER_bl +  fulllewy*new_time + fullcaa*new_time + fulltdp43*new_time +  (1|RID)")
+
+
+# stratify continuous variables and assign treatment based on block randomization
+treatment.enrichment.list <- list()
+for(i in 1:length(baseline.enriched.data)) {
+  sublist <- baseline.enriched.data[[i]]
+  sublist.long <- full.enriched.data[[i]]
+  enrich.data.stratcont <- map2(sublist, strat.continuous, StratifyContVar)
+  full.treatment        <- map2(enrich.data.stratcont, sublist.long, RandomizeTreatment2, stratcolumns = c("PTEDUCAT_bl", "AGE_bl", "PTGENDER", "fulllewy", "fullcaa", "fulltdp43"))
+  treatment.enrichment.list[[i]] <- full.treatment
+  names(treatment.enrichment.list[[i]]) <- names(full.data.list)
+}
+names(treatment.enrichment.list) <- names(baseline.enriched.data)
+
+
+# fit all models for control data
+control.models.list <- list()
+for(i in 1:length(full.control.data)) {
+  sublist <- full.control.data[[i]]
+  control.models.list[[i]]         <- map2(sublist, control.formulas, MapLmer)
+  names(control.models.list[[i]])  <- names(control.formulas)
+}
+names(control.models.list) <- names(control.data)
+
+
+
+#fit all models for enriched data
+enriched.models.list <- list()
+for(i in 1:length(full.enriched.data)) {
+  sublist <- full.enriched.data[[i]]
+  enriched.models.list[[i]] <- map2(sublist, enrichment.formulas, MapLmer)
+  names(enriched.models.list[[i]]) <- names(enrichment.formulas)
+}
+names(enriched.models.list) <- names(full.enriched.data)
+
+
+View(enriched.models.list)
