d77f02bb7baa1f493ace6335948ad9fec9843894
AdamLang96 <45542095+AdamLang96@users.noreply.github.com>
2021-10-08 14:14:01 -0700

    main script runs other scripts in order

---
+++ main.R
@@ -0,0 +1,17 @@
+#run imaging harmonization
+#files saved:
+
+source("/Users/adamgabriellang/Desktop/clinical_trial_sim/harmonize_imaging_for_simulation.R")
+
+#run data preprocessing
+#files saved:
+source("/Users/adamgabriellang/Desktop/clinical_trial_sim/processing_raw_data.R")
+
+#run enrichment processing
+#files saved:
+source("/Users/adamgabriellang/Desktop/clinical_trial_sim/processing_merged_data_for_simulation.R")
+
+#run simulation pre-processing for early AD cohorts
+#files saved:
+source("/Users/adamgabriellang/Desktop/clinical_trial_sim/earlyadsim.R")
+
